b8026c105fe993a234a71990f185976f9bacc57e
mwvasco <mw_vasco@hotmail.com>
2014-01-30 16:30:34 +0000

    Update index.js

--- js/index.js
+++ js/index.js
@@ -67,9 +67,18 @@ xhr.send();
 
 */
 
- $.ajax("https://api.github.com/legacy/repos/search/javascript").done(function(data) {
-     var i, repo;
-     $.each(data.repositories, function (i, repo) {
-        $("#allRepos").append("<p><a href='https://github.com/" + repo.username + "/" + repo.name + "'>" + repo.name + "</a><br>"+ repo.description + "&g\lt;/p>");
-     });
+$('#reposHome').bind('pageinit', function(event) {
+	loadRepos();
 });
+
+function loadRepos() {
+    $.ajax("https://api.github.com/legacy/repos/search/javascript").done(function(data) {
+        var i, repo;
+        $.each(data.repositories, function (i, repo) {
+            $("#allRepos").append("<li><a href='https://github.com/" + repo.username + "/" + repo.name + "'>"
+            + "<h4>" + repo.name + "</h4>"
+            + "<p>" + repo.username + "</p></a></li>");
+        });
+        $('#allRepos').listview('refresh');
+    });
+}

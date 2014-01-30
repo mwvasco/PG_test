ba3f9bf5cfc4c9fea92790ae1b379f6af604ef53
mwvasco <mw_vasco@hotmail.com>
2014-01-30 17:22:26 +0000

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,24 @@
+var db;
+
 $('#reposHome').bind('pageinit', function(event) {
-	loadRepos();
+    loadRepos();
+    db = window.openDatabase("repodb","0.1","GitHub Repo Db", 1000);
+    db.transaction(createDb, txError, txSuccess);
 });
+
+function createDb(tx) {
+    tx.executeSql("DROP TABLE IF EXISTS repos");
+    tx.executeSql("CREATE TABLE repos(user,name)");
+}
+
+function txError(error) {
+    console.log(error);
+    console.log("Database error: " + error);
+}
+
+function txSuccess() {
+    console.log("Success");
+}
 
 function loadRepos() {
     $.ajax("https://api.github.com/legacy/repos/search/javascript").done(function(data) {

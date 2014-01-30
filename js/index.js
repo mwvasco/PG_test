b3775efdda0a0689f38c012de810d6861f098600
mwvasco <mw_vasco@hotmail.com>
2014-01-30 16:22:56 +0000

    Update index.js

--- js/index.js
+++ js/index.js
@@ -64,5 +64,12 @@ xhr.send();
         console.log('Received Event: ' + id);
     }
 };
- */
- 
+
+*/
+
+ $.ajax("https://api.github.com/legacy/repos/search/javascript").done(function(data) {
+     var i, repo;
+     $.each(data.repositories, function (i, repo) {
+        $("#allRepos").append("<p><a href='https://github.com/" + repo.username + "/" + repo.name + "'>" + repo.name + "</a><br>"+ repo.description + "&g\lt;/p>");
+     });
+});
